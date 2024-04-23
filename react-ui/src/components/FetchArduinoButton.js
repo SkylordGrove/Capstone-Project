33c60396595ba2a7954212b2a0d0c37f97d66ebf
SkylordGrove <dggrove@uark.edu>
2024-04-23 03:38:12 -0500

    Small additions to FetchArduinoButton and PortManager

--- react-ui/src/components/FetchArduinoButton.js
+++ react-ui/src/components/FetchArduinoButton.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 // Gets Arduino Data from Electron Wrapper
-const FetchArduinoData = () => {
+const FetchArduinoData = (dataset) => {
     const handleClick = () => {
         console.log('clicked');
-        fetch('http://localhost:8080/arduino').then(res => res.json()).then(data => { console.log(data) });
+        console.log(dataset);
+        setInterval(async () => {
+            try {
+                fetch('http://localhost:8080/arduino')
+                    .then(res => res.json())
+                    //.then(data => console.log(data))
+                    .then(arduinoData => {dataset.dataset[0] = arduinoData;})   //Only updating in this component
+                    .then(console.log(dataset.dataset[0]));                     //need to figure out how to set it in App.js
+            } catch(e) {
+                console.error(e);
+            }
+        }, 1000);
     }
+    
     return (
         <div>
             <button onClick={handleClick}>Get Arduino Data</button>
